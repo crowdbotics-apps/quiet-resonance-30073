713b6d86386a7cb656bcd77019140e85cfb19a45
Crowdbotics <team@crowdbotics.com>
2021-08-24 15:42:50 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,6 @@
+export const globalOptions = {
+  name: "quiet_resonance_30073Identifier",
+  url: "https://quiet_resonance_30073Identifier.botics.co",
+  api: "https://quiet_resonance_30073Identifier.botics.co/api/v1"
+}
+export const modulesOptions = {}
